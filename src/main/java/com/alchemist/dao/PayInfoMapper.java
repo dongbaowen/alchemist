37358172e534238ca8df7cdc0690c45fefe4b842
dongbaowen <dongbaowen92@126.com>
2017-10-15 18:04:05 +0800

    dao

---
+++ src/main/java/com/alchemist/dao/PayInfoMapper.java
@@ -0,0 +1,17 @@
+package com.alchemist.dao;
+
+import com.alchemist.pojo.PayInfo;
+
+public interface PayInfoMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(PayInfo record);
+
+    int insertSelective(PayInfo record);
+
+    PayInfo selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(PayInfo record);
+
+    int updateByPrimaryKey(PayInfo record);
+}
